cc55421b53b688eec788c33b0cb5e4269c41c86b
dosherakxxx <kosachv18@bk.ru>
2025-06-29 19:39:07 +0500

    up

--- system/start_work.bat
+++ system/start_work.bat
@@ -11,13 +11,16 @@ popd
 set "TG_TOKEN=7590659228:AAEz5jSInR7mWXsm0_25PGhkofJ_bNhPxFk"
 set "TG_CHAT_ID=7520366041"
 
-:: === ВРЕМЯ НАЧАЛА (часы:минуты:секунды)
+:: === ПОЛУЧЕНИЕ ДАТЫ (ДД.ММ.ГГГГ)
+for /f "tokens=2 delims==" %%i in ('"wmic os get localdatetime /value"') do set datetime=%%i
+set "DATESTR=%datetime:~6,2%.%datetime:~4,2%.%datetime:~0,4%"
+
+:: === ВРЕМЯ НАЧАЛА (ЧЧ:ММ:СС)
 for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
     set "START=%%a:%%b:%%c"
     set /a sh=1%%a - 100
     set /a sm=1%%b - 100
 )
-
 echo [INFO] Работа началась: %START%
 
 :: === ЗАПУСК GITHUB DESKTOP
@@ -35,13 +38,12 @@ if not errorlevel 1 (
     goto waitloop
 )
 
-:: === ВРЕМЯ ОКОНЧАНИЯ (часы:минуты:секунды)
+:: === ВРЕМЯ ОКОНЧАНИЯ (ЧЧ:ММ:СС)
 for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
     set "END=%%a:%%b:%%c"
     set /a eh=1%%a - 100
     set /a em=1%%b - 100
 )
-
 echo [INFO] Работа завершена: %END%
 
 :: === ПОДСЧЁТ ИЗМЕНЕНИЙ
@@ -53,13 +55,16 @@ for /f "tokens=* delims=" %%i in (git_temp.txt) do (
 )
 del git_temp.txt
 
+:: === ПОДСЧЁТ ВРЕМЕНИ
 set /a workmin=(eh*60+em)-(sh*60+sm)
 if !workmin! lss 0 set /a workmin+=1440
 set /a wh=workmin / 60
 set /a wm=workmin %% 60
 
-set "TEXT=🕒 Отчёт%%0A▶️ Начало: %START%%%0A⏹️ Конец: %END%%%0A⌛ Время: !wh! ч !wm! мин%%0A📊 Изменения: %DIFF_RESULT%"
+:: === СБОРКА СООБЩЕНИЯ С ДАТОЙ
+set "TEXT=🗓 Дата: %DATESTR%%%0A🕒 Отчёт:%0A▶️ Начало: %START%%%0A⏹️ Конец: %END%%%0A⌛ Время: !wh! ч !wm! мин%%0A📊 Изменения: %DIFF_RESULT%"
 
+:: === ОТПРАВКА В TELEGRAM (через curl)
 curl -s -X POST ^
   "https://api.telegram.org/bot%TG_TOKEN%/sendMessage" ^
   -d "chat_id=%TG_CHAT_ID%" ^
