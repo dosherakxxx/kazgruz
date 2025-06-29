0b15ce0beff4cceafa61a4711d1040c96562aa17
dosherakxxx <kosachv18@bk.ru>
2025-06-29 19:48:33 +0500

    last update
    Закончил работу с батником который запускает работу

--- system/start_work.bat
+++ system/start_work.bat
@@ -9,14 +9,12 @@ popd
 set "TG_TOKEN=7590659228:AAEz5jSInR7mWXsm0_25PGhkofJ_bNhPxFk"
 set "TG_CHAT_ID=7520366041"
 
-for /f "tokens=2 delims==" %%i in ('"wmic os get localdatetime /value"') do set datetime=%%i
-set "DATESTR=%datetime:~6,2%.%datetime:~4,2%.%datetime:~0,4%"
-
 for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
     set "START=%%a:%%b:%%c"
     set /a sh=1%%a - 100
     set /a sm=1%%b - 100
 )
+
 echo [INFO] Работа началась: %START%
 
 start "" "C:\Users\%USERNAME%\AppData\Local\GitHubDesktop\GitHubDesktop.exe"
@@ -36,6 +34,7 @@ for /f "tokens=1-3 delims=:.," %%a in ("%TIME%") do (
     set /a eh=1%%a - 100
     set /a em=1%%b - 100
 )
+
 echo [INFO] Работа завершена: %END%
 
 cd /d "%REPO_PATH%"
@@ -51,7 +50,7 @@ if !workmin! lss 0 set /a workmin+=1440
 set /a wh=workmin / 60
 set /a wm=workmin %% 60
 
-set "TEXT=🗓 Дата: %DATESTR%%%0A🕒 Отчёт:%0A▶️ Начало: %START%%%0A⏹️ Конец: %END%%%0A⌛ Время: !wh! ч !wm! мин%%0A📊 Изменения: %DIFF_RESULT%"
+set "TEXT=🕒 Отчёт%%0A▶️ Начало: %START%%%0A⏹️ Конец: %END%%%0A⌛ Время: !wh! ч !wm! мин%%0A📊 Изменения: %DIFF_RESULT%"
 
 curl -s -X POST ^
   "https://api.telegram.org/bot%TG_TOKEN%/sendMessage" ^
